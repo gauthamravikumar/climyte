fc6c8aaa9212353f87bebac4fbe2b4b668834ab0
Gautham Ravikumar <gautilovesyou@gmail.com>
2026-07-23 23:47:55 +1000

    Initial Commit

---
+++ climyte/climyteApp.swift
@@ -0,0 +1,17 @@
+//
+//  climyteApp.swift
+//  climyte
+//
+//  Created by Gautham Ravikumar on 23/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct climyteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
